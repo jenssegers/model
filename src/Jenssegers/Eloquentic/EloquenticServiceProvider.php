31584044d270972073c26d693ac08894083d7024
Jens Segers <segers.jens@gmail.com>
2013-03-30 11:03:51 +0100

    Boot method for service provider

--- src/Jenssegers/Eloquentic/EloquenticServiceProvider.php
+++ src/Jenssegers/Eloquentic/EloquenticServiceProvider.php
@@ -4,6 +4,16 @@ use Illuminate\Support\ServiceProvider;
 
 class EloquenticServiceProvider extends ServiceProvider {
 
+	/**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('jenssegers/chef');
+    }
+
     /**
      * Get the services provided by the provider.
      *
