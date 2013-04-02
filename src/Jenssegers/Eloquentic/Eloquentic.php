72c12f55fe2a4a1d9600ee6c7dc077f94aef4b02
Jens Segers <segers.jens@gmail.com>
2013-04-02 18:24:51 +0200

    JSON output when cast to string

--- src/Jenssegers/Eloquentic/Eloquentic.php
+++ src/Jenssegers/Eloquentic/Eloquentic.php
@@ -30,7 +30,7 @@ abstract class Eloquentic extends ArrayObject {
      * Save a new model and return the instance.
      *
      * @param  array  $attributes
-     * @return \Jenssegers\Eloquentic\Eloquentic
+     * @return Eloquentic
      */
     static public function create($attributes = array())
     {
@@ -42,6 +42,41 @@ abstract class Eloquentic extends ArrayObject {
         }
 
         return $model;
+    }
+
+    /**
+     * Convert the model instance to JSON.
+     *
+     * @param  int  $options
+     * @return string
+     */
+    public function toJson($options = 0)
+    {
+        return json_encode($this, $options);
+    }
+
+    /**
+     * Handle dynamic static method calls into the method.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public static function __callStatic($method, $parameters)
+    {
+        $instance = new static;
+
+        return call_user_func_array(array($instance, $method), $parameters);
+    }
+
+    /**
+     * Convert the model to its string representation.
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        return $this->toJson();
     }
 
 }
