129c23bc12fab26fefc39955b3b238d71c7624a7
Kieran Marshall <kieran.marshall@strawberry.co.uk>
2020-04-29 21:51:13 +0100

    Add custom casts support.

---
+++ src/Contracts/CastsAttributes.php
@@ -0,0 +1,26 @@
+<?php namespace Jenssegers\Model\Contracts;
+
+interface CastsAttributes
+{
+    /**
+     * Transform the attribute from the underlying model values.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function get($model, string $key, $value, array $attributes);
+
+    /**
+     * Transform the attribute to its underlying model values.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return array
+     */
+    public function set($model, string $key, $value, array $attributes);
+}
